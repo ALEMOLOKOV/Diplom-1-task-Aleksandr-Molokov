bbb8f87ca7981520307ac1778378078d6e128d02
Alexandr Molokov <sanmol010788@gmail.com>
2023-11-14 23:23:02 +0300

    1 задание terraform

--- diplom/task-1-tf-YC/providers.tf
+++ diplom/task-1-tf-YC/providers.tf
@@ -5,7 +5,18 @@ terraform {
     }
   }
   required_version = ">=0.47"
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "alemolokov-01-12-2023"
+    region     = "ru-central1"
+    key        = "default.tfstate"
+    access_key = var.sa-access-key
+    secret_key = var.sa-secret-key
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
   }
+}
 provider "yandex" {
    token     = var.yc_token
    cloud_id  = var.cloud-id
